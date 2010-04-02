a3f01c2d623be6a1b865507d2ae363c24eabe1f8
TatsuyaLam <lamtl@localhost.localdomain>
2010-04-02 16:00:50 +0700

    Update user model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  layout "master"
+  
   # GET /users
   # GET /users.xml
   def index
@@ -81,5 +83,62 @@ class UsersController < ApplicationController
       format.html { redirect_to(users_url) }
       format.xml  { head :ok }
     end
+  end
+  
+  # Action to login
+  def login
+    session[:username] = nil
+    if request.post?
+      user = User.authenticate(params[:username], params[:password])
+      if user
+        # Set user sesssion  
+        session[:username] = user.username
+        session[:user_id] = user.id
+        session[:admin] = true if user.is_admin
+        # Set current user
+        User.current_user = user
+        flash[:notice] = "Welcome #{user.username}!"
+        redirect_to administration_url
+      else
+        flash[:notice] = "Username or Password is invalid!"
+      end
+    end
+  end
+
+  # Action to logout
+  def logout
+    reset_session
+    flash[:notice] = "Logout successfully!"
+    redirect_to login_url
+  end
+
+  # Action to change password
+  def change_password
+    if request.post?
+      user = User.authenticate(session[:username], params[:old_password])
+      if user
+        unless params[:new_password] == params[:new_password_confirmation]
+          flash[:notice] = "Password confirmation does not match."
+          return
+        end
+        user.password = params[:new_password]
+        user.save
+        flash[:notice] = "Change password successfully!"
+      else
+        flash[:notice] = "Password is invalid!"
+      end
+    end
+  end
+
+  # Action to reset password
+  def reset_password
+    begin
+      user = User.find(params[:id])
+      user.reset_password
+      flash[:notice] = "Reset password of #{user.name} successfully!"
+    rescue Exception => e
+      flash[:notice] = e.message
+    end
+    redirect_to :action => :index
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,103 @@ class User < ActiveRecord::Base
               :foreign_key => "created_by"    
   
   # Validation
-  validates_presence_of :username, :fullname, :email
-  validates_uniqueness_of :username                
+  validates_presence_of   :username, :fullname, :email
+  validates_uniqueness_of :username     
+  
+  # Property
+  cattr_accessor            :current_user
+  attr_accessor             :password_confirmation
+  validates_confirmation_of :password
+
+  # Property password
+  def password
+    @password
+  end
+
+  # Property password=
+  def password=(pwd)
+    @password = pwd
+    return if pwd.blank?
+    create_new_salt
+    self.hash_password = User.encrypted_password(self.password, self.salt)
+  end
+
+  # Static authenticate function
+  def self.authenticate(name, password)
+    user = self.find_by_username(name)
+    if user
+      expected_password = encrypted_password(password, user.salt)
+      if user.hashed_password != expected_password
+        user = nil
+      end
+    end
+    user
+  end
+
+  # Overwrite after_destroy to check user exist
+  def after_destroy
+    if User.count.zero?
+      raise "Can not delete all users!"
+    end
+  end
+
+  # Overwrite before_detroy to check assigend user
+  def before_destroy
+      raise "Can not delete current user!" if has_been_assigned?
+  end  
+
+  # Function to reset password
+  # Set new password = username
+  def reset_password
+    self.password = self.username
+    self.save
+  end
+
+  # Check existed user by username
+  # return: nil, if user was not found
+  #         user, if user exist
+  def self.is_exist?(username)
+    return User.find(:first,
+                     :conditions => ["username = ?", username])
+  end
+
+  # Check role of user by role id
+  # return: nil, if user does not has role
+  def self.has_role_id?(user_id, role_id)
+    return UserRole.find(:all, :conditions => ["user_id = ? and role_id = ?", user_id, role_id])
+  end
+
+  # Check role of user by role name
+  # return: nil, if user does not has role
+  def self.has_role?(user_id, role_name)
+    return UserRole.find(:all,
+                         :joins => "INNER JOIN roles on user_roles.role_id = roles.id",
+                         :conditions => ["user_roles.user_id = ? and roles.name = ?", user_id, role_name])
+  end
+
+  # Get all users with role
+  def self.all_users_with_role(role_name)
+    return User.find(:all,
+                     :joins => "INNER JOIN user_roles ON users.id = user_roles.user_id
+                                INNER JOIN roles ON user_roles.role_id = roles.id",
+                     :conditions => ["roles.name = ?", role_name],
+                     :select => "users.*")
+  end
+
+  private
+  # Function to encrypt password
+  def self.encrypted_password(password, salt)
+    string_to_hash = password + "wibble" + salt
+    Digest::SHA1.hexdigest(string_to_hash)
+  end
+
+  # Create new salt
+  def create_new_salt
+    self.salt = self.object_id.to_s + rand.to_s
+  end
+  
+  # Check assigned user
+  def has_been_assigned?
+    return false
+  end
 end
